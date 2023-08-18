b89b5703bc78e084323dd2716fe614a67d279890
Artur <74054908+EncouragedEntity@users.noreply.github.com>
2023-08-18 22:14:54 +0300

    Update meal_item.dart

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -30,6 +30,7 @@ class MealItem extends StatelessWidget {
         child: Stack(
           children: [
             Hero(
+              
               tag: meal.id,
               child: FadeInImage(
                 placeholder: MemoryImage(kTransparentImage),
